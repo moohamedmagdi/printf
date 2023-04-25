03b24d687dd1b04b6df8f49f31437a6729518878
youssef-elmelegy <youssefelmelegy999@gmail.com>
2023-04-25 12:35:47 -0700

    update

--- _put.c
+++ _put.c
@@ -9,6 +9,7 @@
 int _puts(char *str)
 {
 	char *a = str;
+
 	while (*str)
 		_putchar(*str++);
 	return (str - a);
@@ -25,6 +26,7 @@ int _putchar(int c)
 {
 	static int i;
 	static char buf[OUTPUT_BUF_SIZE];
+
 	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
 	{
 		write(1, buf, i);
