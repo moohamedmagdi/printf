95a47e1e29a1b7fa9d45cc4beb545d8f8ccf93c8
moohamedmagdi <moohamedmagdi@gmail.com>
2023-04-25 11:55:36 -0700

    specifier betty

--- specifier.c
+++ specifier.c
@@ -9,25 +9,25 @@
 
 int (*get_specifier(char *S))(va_list ap, params_t *params)
 {
-        specifier_t specifiers[] = {
-                {"c", printf_char},
-                {"d", printf_char},
-                {"i", printf_char},
-                {"s", printf_char},
-                {"%", printf_char},
-                {NULL, NULL}
-        };
-        int i = 0;
+	specifier_t specifiers[] = {
+		{"c", printf_char},
+		{"d", printf_char},
+		{"i", printf_char},
+		{"s", printf_char},
+		{"%", printf_char},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-        while (specifiers[i].specifier)
-        {
-                if (*s == specifiers[i].specifier[0])
-                {
-                        return (specifiers[i].f);
-                }
-                i++;
-        }
-        return (NULL);
+	while (specifiers[i].specifier)
+	{
+		if (*s == specifiers[i].specifier[0])
+		{
+			return (specifiers[i].f);
+		}
+		i++;
+	}
+	return (NULL);
 }
 
 /**
@@ -41,9 +41,9 @@ int (*get_specifier(char *S))(va_list ap, params_t *params)
 
 int get_print_func(char *s, va_list ap, params_t *params)
 {
-        int (*f)(va_list, params_t *) = get_specifier(s);
+	int (*f)(va_list, params_t *) = get_specifier(s);
 
-        if (f)
-                return (f(ap, params));
-        return (0);
+	if (f)
+		return (f(ap, params));
+	return (0);
 }
