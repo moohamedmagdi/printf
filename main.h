4575c6a4205b84257eb93c106597318dbb119975
moohamedmagdi <moohamedmagdi@gmail.com>
2023-04-25 13:26:40 -0700

    adding prototypes

--- main.h
+++ main.h
@@ -62,6 +62,11 @@ int printf_int(va_list ap, params_t *params);
 int printf_char(va_list ap, params_t *params);
 int printf_string(va_list ap, params_t *params);
 
+int print_octal(va_list ap, params_t *params);
+int print_binary(va_list ap, params_t *params);
+int print_hex(va_list ap, params_t params); /* hex lowercase */
+int print_HEX(va_list ap, params_t params); /* hex uppercase */
+int print_octal(va_list ap, params_t *params);
 
 char *convert(long int num, int base, int flags, params_t *params);
 int print_unsigned(va_list ap, params_t *params);
