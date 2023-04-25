cfa11297b99e9e923242c7f6c5d40c21414c5c68
moohamedmagdi <moohamedmagdi@gmail.com>
2023-04-25 12:50:43 -0700

    main adding prototype

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _PRINTF_H
+#define _PRINTF_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,6 +12,9 @@
 
 #define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 
+#define CONVERT-LOWERCASE     1
+#define CONVERT-UNSIGNED      2
+
 
 /**
  * struct parameters - params struct
@@ -21,7 +24,8 @@
 
 typedef struct parameters
 {
-	unsigned int unsign : 1;
+	unsigned int unsign :		1;
+
 	unsigned int precision;
 } params_t;
 
@@ -43,6 +47,10 @@ int printf_int(va_list ap, params_t *params);
 int printf_char(va_list ap, params_t *params);
 int printf_string(va_list ap, params_t *params);
 
+char *convert(long int num, int base, int flags, params_t *params);
+int print_unsigned(va_list ap, params_t *params);
+int print_address(va_list ap, params_t params);
+
 int _putchar(int c);
 int _puts(char *str);
 
@@ -50,8 +58,16 @@ int _printf(const char *format, ...);
 
 void init_params(params_t *params, va_list ap);
 
+int get_modifier(char *s, params_t *params);
+int print_from_to(char *start, char *stop,char *except);
 int (*get_specifier(char *s))(va_list ap, params_t *params);
 int get_print_func(char *s, va_list ap, params_t *params);
+
+int _isdigit(int c);
+int _strlen(char *s);
+int print_number(char *str, params_t *params);
+int print_number_right_shift(char *str, params_t *params);
+int print_number_left_shift(char *str, params_t *params);
 
 #endif
 
