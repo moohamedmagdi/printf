e699fef74d3641575df81b93295ee8de7f871c00
moohamedmagdi <moohamedmagdi@gmail.com>
2023-04-25 13:44:21 -0700

    adding to file specifier

--- specifier.c
+++ specifier.c
@@ -11,10 +11,19 @@ int (*get_specifier(char *S))(va_list ap, params_t *params)
 {
 	specifier_t specifiers[] = {
 		{"c", printf_char},
-		{"d", printf_char},
-		{"i", printf_char},
-		{"s", printf_char},
-		{"%", printf_char},
+		{"d", printf_int},
+		{"i", printf_int},
+		{"s", printf_string},
+		{"%", printf_percent},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"u", print_unsigned},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{"p", print_address},
+		{"S", print_S},
+		{"r", print_rev},
+		{"R", print_rot13},
 		{NULL, NULL}
 	};
 	int i = 0;
@@ -46,4 +55,87 @@ int get_print_func(char *s, va_list ap, params_t *params)
 	if (f)
 		return (f(ap, params));
 	return (0);
+}
+
+/**
+ * get_flag - finds the flag func
+ * @s: the format string
+ * @params: the parameters struct
+ *
+ * Return: if flag was valid
+ */
+
+int get_flag(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+		case '+':
+			i = params->plus_flag = 1;
+			break;
+		case ' ':
+			i = params->space_flag = 1;
+			break;
+		case '#':
+			i = params->hashtag_flag = 1;
+			break;
+		case '-':
+			i = params->minus_flag = 1;
+			break;
+		case '0':
+			i = params->zero_flag = 1;
+			break;
+	}
+	return (i);
+}
+
+/**
+ * get_modifier - find modifier func
+ * @s: format string
+ * @params: parameters struct
+ *
+ * Return: if valid
+ */
+
+int get_modifier(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+	case 'h':
+		i = params->h_modifier = 1;
+		break;
+	case 'l':
+		i = params->l_modifier = 1;
+		break;
+	}
+	return (i);
+}
+
+/**
+ * get_width - width from format string
+ * @s: format string
+ * @params: parameters struct
+ * @ap: argument pointer
+ *
+ * Return: new pointer
+ */
+char *get_width(char *s, params_t *params, va_list ap)
+{
+	int d = 0;
+
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->width = d;
+	return (s);
 }
