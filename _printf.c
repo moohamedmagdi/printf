276426359aa40c1c329ae24a662b16b1634f3ae5
youssef-elmelegy <youssefelmelegy999@gmail.com>
2023-04-25 17:27:48 -0700

    fix

--- _printf.c
+++ _printf.c
@@ -39,12 +39,7 @@ int _printf(const char *format, ...)
 		if (get_modifier(p, &params))
 			p++;
 		if (!get_specifier(p))
-			sum += print_from_to(start, p,
-<<<<<<< HEAD
-				       	params.l_modifier || params.h_modifier ? p - 1 : 0);
-=======
-			    params.l_modifier || params.h_modifier ? p - 1 : 0);
->>>>>>> 960cb9ce113abf39d93d9c14a0f5b1885be57b6d
+			sum += print_from_to(start, p, params.1_modifier || params.h_modifier ? p - 1 : 0);
 		else
 			sum += get_print_func(p, ap, &params);
 	}
